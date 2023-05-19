176de8d8519c19e9c4110e04ee5a6f67b608abc0
naimur2001 <naimurrahman18thapril@gmail.com>
2023-05-19 22:24:37 +0600

    delete method done

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ app.get('/toycars/:id',async(req,res)=>{
   const result=await toyCarCollection.findOne(query);
   res.send(result)
 })
+// delete method
+app.delete('/toycars/:id',async(req,res)=>{
+  const id=req.params.id;
+  const query={_id:new ObjectId(id)}
+  const result=await toyCarCollection.deleteOne(query);
+  res.send(result)
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
